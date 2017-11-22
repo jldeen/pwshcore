9eb8376656f842b4e956606aa39848d58a88564a
jldeen <jedeen@outlook.com>
2017-11-22 00:30:44 -0800

    sudo fix

--- install.sh
+++ install.sh
@@ -157,9 +157,9 @@ function installPSCore14 {
 function installPSCore16 {
     {
         # Import the public repository GPG keys
-        curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+        curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo -S <<< $psw apt-key add -
         # Register the Microsoft Ubuntu repository
-        curl -s https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+        curl -s https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo -S <<< $psw tee /etc/apt/sources.list.d/microsoft.list
         # Update apt-get
         sudo -S <<< $psw apt-get update
         # Install PowerShell
