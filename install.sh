c53c6b1d60251f0b88231cdfe2e540b294e6cf22
jldeen <jedeen@outlook.com>
2017-11-22 13:41:18 -0800

    fix

--- install.sh
+++ install.sh
@@ -251,6 +251,7 @@ function installAzureRM {
 function installAzCli {
     {
         function rpmCliCheck {
+            {
             rpm -qa '*release*' > /dev/null 2>&1 
             if [ $? -eq 0 ]; then
                 # sudo -S - auth sudo in advance
@@ -260,7 +261,8 @@ function installAzCli {
                 yum check-update
                 sudo yum install azure-cli
             fi
-        } | whiptail --title "PowerShell Core Installer" --gauge "Installing Azure CLI 2.0 for RHEL" 6 60 0 
+            } | whiptail --title "PowerShell Core Installer" --gauge "Installing Azure CLI 2.0 for RHEL" 6 60 0
+        }  
         rpmCliCheck
         # sudo -S - auth sudo in advance
         sudo -S <<< $psw ls > /dev/null 2>&1
