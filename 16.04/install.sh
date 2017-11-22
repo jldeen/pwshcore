bca736e3ddff2b4623d123485f3e54b7cb267836
jldeen <jedeen@outlook.com>
2017-11-21 16:41:21 -0800

    test

--- 16.04/install.sh
+++ 16.04/install.sh
@@ -1,62 +1,115 @@
 #!/bin/bash
-echo "This script will install the latest version of PowerShell Core on your Ubuntu 16.04 system...."
-echo .
 
-# Import the public repository GPG keys
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+## Test environment Selector
+    DISTROS=$(whiptail --title "PowerShell Core Installer" --radiolist \
+    "What is your environment?" 15 60 4 \
+    "Ubuntu" "14.04" OFF \
+    "Ubuntu" "16.04" OFF \
+    "Ubuntu" "17.04" OFF \
+    "macOS" OFF 3>&1 1>&2 2>&3)
+    
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        echo "The environment you selected for install is:" $DISTROS
+    else
+        echo "You chose Cancel."
+    fi
 
-# Register the Microsoft Ubuntu repository
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+function installPSCore14 {
+    echo "This script will install the latest version of PowerShell Core on your Ubuntu 16.04 system...."
+    echo .
 
-# Update apt-get
-sudo apt-get update
+    # Import the public repository GPG keys
+    curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
-# Install PowerShell
-echo .
-sudo apt-get install -y powershell && echo "The latest version of Powershell Core has been installed..."
-echo .
+    # Register the Microsoft Ubuntu repository
+    curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
 
-echo "This script will now install the AzureRM Modules..."
-echo .
+    # Update apt-get
+    sudo apt-get update
 
-#Azure RM NetCore Preview Module Install
+    # Install PowerShell
+    echo .
+    sudo apt-get install -y powershell && echo "The latest version of Powershell Core has been installed..."
+    echo .
 
-{
+}
+function installPSCore16 {
+    echo "This script will install the latest version of PowerShell Core on your Ubuntu 16.04 system...."
+    echo .
+
+    # Import the public repository GPG keys
+    curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+    # Register the Microsoft Ubuntu repository
+    curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+
+    # Update apt-get
+    sudo apt-get update
+
+    # Install PowerShell
+    echo .
+    sudo apt-get install -y powershell && echo "The latest version of Powershell Core has been installed..."
+    echo .
+
+}
+function installPSCore17 {
+    echo "This script will install the latest version of PowerShell Core on your Ubuntu 16.04 system...."
+    echo .
+
+    # Import the public repository GPG keys
+    curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+    # Register the Microsoft Ubuntu repository
+    curl https://packages.microsoft.com/config/ubuntu/17.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+
+    # Update apt-get
+    sudo apt-get update
+
+    # Install PowerShell
+    echo .
+    sudo apt-get install -y powershell && echo "The latest version of Powershell Core has been installed..."
+    echo .
+
+}
+function installAzureRM {
+    echo "This script will now install the AzureRM Modules..."
+    echo .
+
+    #Azure RM NetCore Preview Module Install
     sudo pwsh -Command Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     sudo pwsh -Command Install-Module -Name AzureRM.Netcore
     sudo pwsh -Command Import-Module -Name AzureRM.Netcore
-    for ((i = 0 ; i <= 100 ; i+=20)); do
-        sleep 1
-        echo $i
-    done
-} | whiptail --gauge "Please wait while installing..." 6 60 0
 
-if [[ $? -eq 0 ]]
-    then
-        echo "Successfully installed PowerShell Core with AzureRM NetCore Module."
-    else
-        echo "PowerShell Core with AzureRM NetCore Module did not install successfully." >&2
-fi
-
-#Install Azure CLI 2.0
-#Address https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
-#Github https://github.com/Azure/azure-cli/releases
-
-read -p "Would you also like to install Azure CLI? y/n" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
-     sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-     sudo apt-get install apt-transport-https
-     sudo apt-get update && sudo apt-get install azure-cli
     if [[ $? -eq 0 ]]
+        then
+            echo "Successfully installed PowerShell Core with AzureRM NetCore Module."
+        else
+            echo "PowerShell Core with AzureRM NetCore Module did not install successfully." >&2
+    fi
+}
+
+function installAzCli {
+    #Install Azure CLI 2.0
+    #Address https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
+    #Github https://github.com/Azure/azure-cli/releases
+
+    read -p "Would you also like to install Azure CLI? y/n" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        echo "Successfully installed Azure CLI 2.0"
-    else
-        echo "Azure CLI 2.0 not installed successfully" >&2
-fi
-else 
-    echo "You chose not to install Azure CLI 2.0... Exiting now."
-fi
+        echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+        sudo tee /etc/apt/sources.list.d/azure-cli.list
+        sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+        sudo apt-get install apt-transport-https
+        sudo apt-get update && sudo apt-get install azure-cli
+        if [[ $? -eq 0 ]]
+        then
+            echo "Successfully installed Azure CLI 2.0"
+        else
+            echo "Azure CLI 2.0 not installed successfully" >&2
+    fi
+    else 
+        echo "You chose not to install Azure CLI 2.0... Exiting now."
+    fi
+}
