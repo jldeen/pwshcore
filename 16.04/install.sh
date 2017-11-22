025bbea115e6c6dbac4c55cabbd37ae29d4fba5b
jldeen <jedeen@outlook.com>
2017-11-21 17:15:40 -0800

    changes

--- 16.04/install.sh
+++ 16.04/install.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
 
-whiptail --title "PowerShell Core Installer" --yesno "Install latest version of PowerShell Core" 8 78
- 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    status="0"
-    while [ "$status" -eq 0 ]  
-    do
-        choice=$(whiptail --title "Environment Selection" --menu "Please choose your environment" 16 78 5 \
-        "ubuntu14" "14.04" \
-        "ubuntu16" "16.04" \
-        "ubuntu17" "17.04" \
-        "macOS" "10.12+" 3>&2 2>&1 1>&3) 
-         
-        # Change to lower case and remove spaces.
-        option=$(echo $choice | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
-        case "${option}" in
-            ubuntu14) installPSCore14 | whiptail --title "PowerShell Core Install"
-            ;;
-            ubuntu16) installPSCore16 | whiptail --title "PowerShell Core Install"
-            ;;
-            ubuntu17) installPSCore17 | whiptail --title "PowerShell Core Install"
-            ;;
-            macos) installmacOS
-            ;;
-            *) whiptail --title "PowerShell Core Installer" --msgbox "You cancelled or have finished." 8 78
-                status=1
-                exit
-            ;;
-        esac
-        exitstatus1=$status1
-    done
-else
-    whiptail --title "PowerShell Core Installer" --msgbox "You chose not to proceed." 8 78
-    exit
-fi
-
 function installPSCore14 {
     echo "This script will install the latest version of PowerShell Core on your Ubuntu 16.04 system...."
     echo .
@@ -149,3 +113,39 @@ function installAzCli {
         echo "You chose not to install Azure CLI 2.0... Exiting now."
     fi
 }
+
+whiptail --title "PowerShell Core Installer" --yesno "Install latest version of PowerShell Core" 8 78
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    status="0"
+    while [ "$status" -eq 0 ]  
+    do
+        choice=$(whiptail --title "Environment Selection" --menu "Please choose your environment" 16 78 5 \
+        "ubuntu14" "14.04" \
+        "ubuntu16" "16.04" \
+        "ubuntu17" "17.04" \
+        "macOS" "10.12+" 3>&2 2>&1 1>&3) 
+         
+        # Change to lower case and remove spaces.
+        option=$(echo $choice | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
+        case "${option}" in
+            ubuntu14) installPSCore14 | whiptail --title "PowerShell Core Install"
+            ;;
+            ubuntu16) installPSCore16
+            ;;
+            ubuntu17) installPSCore17 | whiptail --title "PowerShell Core Install"
+            ;;
+            macos) installmacOS
+            ;;
+            *) whiptail --title "PowerShell Core Installer" --msgbox "You cancelled or have finished." 8 78
+                status=1
+                exit
+            ;;
+        esac
+        exitstatus1=$status1
+    done
+else
+    whiptail --title "PowerShell Core Installer" --msgbox "You chose not to proceed." 8 78
+    exit
+fi
