efb417fc477db975bace4e0cc6a874e690c670d4
jldeen <jedeen@outlook.com>
2017-11-21 16:02:51 -0800

    test

--- 16.04/install.sh
+++ 16.04/install.sh
@@ -12,6 +12,7 @@ curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /et
 sudo apt-get update
 
 # Install PowerShell
+echo .
 sudo apt-get install -y powershell && echo "The latest version of Powershell Core has been installed..."
 echo .
 
@@ -22,6 +23,25 @@ echo .
 sudo pwsh -Command Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 sudo pwsh -Command Install-Module -Name AzureRM.Netcore
 sudo pwsh -Command Import-Module -Name AzureRM.Netcore
+    phases=( 
+    'Installing PowerShell Core....'
+    'Installing AzureRM Modules...'
+    'Importing Azure RM Modules...'
+    'Installing Azure CLI 2.0...'
+    )   
+
+    for i in $(seq 1 100); do  
+        sleep 0.1
+
+        if [ $i -eq 100 ]; then
+            echo -e "XXX\n100\nDone!\nXXX"
+        elif [ $(($i % 25)) -eq 0 ]; then
+            let "phase = $i / 25"
+            echo -e "XXX\n$i\n${phases[phase]}\nXXX"
+        else
+            echo $i
+        fi 
+    done | whiptail --title 'PowerShell Core Setup' --gauge "${phases[0]}" 6 60 0
 
 if [[ $? -eq 0 ]]
     then
