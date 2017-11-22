2cdfa7feafbfc9fd8a33f0f7328ef3701eff445b
jldeen <jedeen@outlook.com>
2017-11-22 12:28:34 -0800

    changes for 17

--- install.sh
+++ install.sh
@@ -210,7 +210,7 @@ function installPSCore17 {
        # sudo -S - auth sudo in advance
         sudo -S <<< $psw ls > /dev/null 2>&1
         # Import the public repository GPG keys
-        curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - > /dev/null 2>&1
+        curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
         # Register the Microsoft Ubuntu repository
         curl -s https://packages.microsoft.com/config/ubuntu/17.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
         # Update apt-get
