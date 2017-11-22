1b71f40a4c562319c069f9f1439b68859f6027a0
jldeen <jedeen@outlook.com>
2017-11-22 09:55:05 -0800

    fix azcli install

--- install.sh
+++ install.sh
@@ -230,8 +230,8 @@ function installAzCli {
         # sudo -S - auth sudo in advance
         sudo -S <<< $psw ls > /dev/null 2>&1
         echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
-        sudo /etc/apt/sources.list.d/azure-cli.list
-        sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893 > /dev/null 2>&1
+        sudo tee /etc/apt/sources.list.d/azure-cli.list
+        sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
         sudo apt-get install apt-transport-https
         sudo apt-get update && sudo apt-get install azure-cli
         if [[ $? -eq 0 ]]
