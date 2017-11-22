7326341422e85b5c2ddf18b7db2081740a88ed06
jldeen <jedeen@outlook.com>
2017-11-22 10:30:19 -0800

    test

--- install.sh
+++ install.sh
@@ -61,8 +61,7 @@ function envselctall {
 function optInstall {
     choice=$(whiptail --title "Optional Features" --checklist "Please select which features you would like to install" 20 78 15 \
     "azureRM" "AzureRM Modules" ON \
-    "azureCli" "Azure CLI 2.0" OFF \
-    "back" "Back to main menu" OFF 3>&2 2>&1 1>&3) 
+    "azureCli" "Azure CLI 2.0" OFF 3>&2 2>&1 1>&3) 
         case $choice in
             azureRM) installAzureRM
             ;;
