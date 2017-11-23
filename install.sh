61629ad31b20c9cbe5d7659d13e30ba6a9d18fcd
jldeen <jedeen@outlook.com>
2017-11-22 20:02:26 -0800

    noninteractive updates

--- install.sh
+++ install.sh
@@ -263,7 +263,7 @@ function installAzCli {
         echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
         sudo tee /etc/apt/sources.list.d/azure-cli.list > /dev/null 2>&1
         sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893 > /dev/null 2>&1
-        sudo apt-get install apt-transport-https
+        sudo apt-get install -y apt-transport-https
         sudo apt-get update && sudo apt-get install azure-cli
         if [[ $? -eq 0 ]]
         then
