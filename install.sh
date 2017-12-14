14b25e0de72f94071310abb40df2029b42d797e8
jldeen <jedeen@outlook.com>
2017-12-14 09:49:25 -0800

    test

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ if [ $exitstatus = 0 ]; then
 fi
 function psswd {
     # password capture
-    psw=$(whiptail --title "PowerShell Core Install | Sudo Password Capture" --passwordbox "Sudo is required to install PowerShell Core. Please enter your sudo password to proceed with the install." 10 60 3>&1 1>&2 2>&3)
+    whiptail --title "PowerShell Core Install | Sudo Password Capture" --passwordbox "Sudo is required to install PowerShell Core. Please enter your sudo password to proceed with the install." 10 60 3>&1 1>&2 2>&3
     exitstatus=$?
         if [ $exitstatus = 0 ]; then
             whiptail --title "PowerShell Core Installation" --msgbox "Sudo password captured successfully!" 8 78
