ba85e792126d182ac67ed359cfe53365eeb3f14a
jldeen <jedeen@outlook.com>
2017-12-27 13:01:50 -0800

    Update whiptail check

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 # script version
 ver=1.2
-#Suse Pre-Req install
+#SUSE Whiptail Pre-Req Install 
 if [ -f /etc/SuSE-release ] ; then
     whiptail > /dev/null 2>&1
     exitstatus=$?
     if [ $exitstatus != 0 ]; then
-    sudo zypper --non-interactive install newt
+    echo "Whiptail not found. Whiptail is required for this PowerShell Installation GUI Script. Now installing Newt package with Whiptail..."
+    echo
+    echo
+    echo "Please wait..."
+    sudo zypper --non-interactive install newt && echo "Successfully installed Newt with Whiptail. PowerShell Core Install script will now proceed."
+    echo 
+    echo
     fi
 fi
 
+### Begin GUI install...
 # passwd capture function
 function capturePass {
     # password capture
